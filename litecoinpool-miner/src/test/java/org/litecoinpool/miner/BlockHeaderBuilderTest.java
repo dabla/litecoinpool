81bf061f8c510fa21f21e6db855d00e709c34ac4
David Blain <david.blain@infrabel.be>
2018-01-22 16:45:55 +0100

    Added scrypt algorithm to Crypto class

--- litecoinpool-miner/src/test/java/org/litecoinpool/miner/BlockHeaderBuilderTest.java
+++ litecoinpool-miner/src/test/java/org/litecoinpool/miner/BlockHeaderBuilderTest.java
@@ -1,7 +1,10 @@
 package org.litecoinpool.miner;
 
+import static org.apache.commons.codec.binary.Hex.decodeHex;
+import static org.apache.commons.codec.binary.Hex.encodeHexString;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.litecoinpool.miner.BlockHeaderBuilder.aBlockHeader;
+import static org.litecoinpool.miner.Crypto.crypto;
 
 import org.junit.Test;
 
@@ -16,5 +19,6 @@ public class BlockHeaderBuilderTest {
 					  				  .build();
 		
 		assertThat(actual).isEqualTo("020000000413cf7d02794eb094cd66404e46aa816679e12f6fdf9ee11f0e97337702b6e00b1edc1ccf82d3214423fc68234f4946119e39df2cc2137e31ebc186191d54229b8f1753dbdf441b00000000");
+		assertThat(encodeHexString(crypto().scrypt(decodeHex(actual)))).isEqualTo("f6f13e350aa4f251e192ab8a78690ee99f1cc2d930d4ae16c4172a0a8aefddd0");
 	}
 }
---
+++ litecoinpool-miner/src/test/java/org/litecoinpool/miner/CryptoTest.java
@@ -0,0 +1,15 @@
+package org.litecoinpool.miner;
+
+import static org.apache.commons.codec.binary.Hex.decodeHex;
+import static org.apache.commons.codec.binary.Hex.encodeHexString;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.litecoinpool.miner.Crypto.crypto;
+
+import org.junit.Test;
+
+public class CryptoTest {
+	@Test
+	public void scrypt() throws Exception {
+		assertThat(encodeHexString(crypto().scrypt(decodeHex("020000000413cf7d02794eb094cd66404e46aa816679e12f6fdf9ee11f0e97337702b6e00b1edc1ccf82d3214423fc68234f4946119e39df2cc2137e31ebc186191d54229b8f1753dbdf441b00000000")))).isEqualTo("f6f13e350aa4f251e192ab8a78690ee99f1cc2d930d4ae16c4172a0a8aefddd0");
+	}
+}
