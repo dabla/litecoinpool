3b92d53f05a2155d958ab57fd2f8035f47e9e510
David Blain <david.blain@infrabel.be>
2018-01-23 08:17:28 +0100

    Use ByteAsserter in HasherTest

--- litecoinpool-miner/src/test/java/org/litecoinpool/miner/HasherTest.java
+++ litecoinpool-miner/src/test/java/org/litecoinpool/miner/HasherTest.java
@@ -1,7 +1,6 @@
 package org.litecoinpool.miner;
 
-import static org.apache.commons.codec.binary.Hex.encodeHexString;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.litecoinpool.miner.ByteAsserter.assertThat;
 import static org.litecoinpool.miner.HasherBuilder.aHasher;
 import static org.litecoinpool.miner.HasherBuilderTest.EXTRANONCE1;
 import static org.litecoinpool.miner.HasherBuilderTest.EXTRANONCE2;
@@ -20,7 +19,7 @@ public class HasherTest {
 				 .withExtranonce2(EXTRANONCE2)
 				 .build();
 		
-		assertThat(encodeHexString(actual.getCoinbase())).isEqualTo("280b3927f6763b1ed90cae2a3cef4d27c743f6a7d91e3901dc3816a46acacf36");
-		assertThat(encodeHexString(actual.getMerkleRoot())).isEqualTo("0b1edc1ccf82d3214423fc68234f4946119e39df2cc2137e31ebc186191d5422");
+		assertThat(actual.getCoinbase()).isEqualTo("280b3927f6763b1ed90cae2a3cef4d27c743f6a7d91e3901dc3816a46acacf36");
+		assertThat(actual.getMerkleRoot()).isEqualTo("0b1edc1ccf82d3214423fc68234f4946119e39df2cc2137e31ebc186191d5422");
 	}
 }
