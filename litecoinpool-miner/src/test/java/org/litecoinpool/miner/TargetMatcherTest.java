c74b1def0d61c052687cb1c1d61e070ac050caa5
David Blain <david.blain@infrabel.be>
2018-02-04 18:06:31 +0100

    dbl: reorganized TargetMatcherTest

--- litecoinpool-miner/src/test/java/org/litecoinpool/miner/TargetMatcherTest.java
+++ litecoinpool-miner/src/test/java/org/litecoinpool/miner/TargetMatcherTest.java
@@ -24,7 +24,7 @@ public class TargetMatcherTest {
 	
 	@Test
 	public void getTarget() throws Exception {
-		assertThat(TargetMatcher.withDifficulty(1).getTarget()).isEqualTo(new BigDecimal(Nonce.fromHex("0000ffff00000000000000000000000000000000000000000000000000000000")));
+		assertThat(TargetMatcher.withDifficulty(1).getTarget()).isEqualTo(TargetMatcher.MAX_TARGET);
 		assertThat(TargetMatcher.withDifficulty(32).getTarget()).isEqualTo(new BigDecimal(Nonce.fromHex("000007fff8000000000000000000000000000000000000000000000000000000")));
 		assertThat(TargetMatcher.withDifficulty(Nonce.fromHex("0000003cc3660000000000000000000000000000000000000000000000000000")).getTarget()).isEqualTo(new BigDecimal(1078.5297507748264));
 		assertThat(TargetMatcher.withDifficulty("1b3cc366").getTarget()).isEqualTo(new BigDecimal(1078.5297507748264));
@@ -36,7 +36,7 @@ public class TargetMatcherTest {
 		assertThat(TargetMatcher.withDifficulty(32)
 								.matches("f6f13e350aa4f251e192ab8a78690ee99f1cc2d930d4ae16c4172a0a8aefddd0")).isFalse();
 		assertThat(TargetMatcher.withDifficulty(32)
-			    				.matches("7441207b6390054623bc5e659ffe2581356dafc5ec41db44d27de85035000000")).isTrue();
+								.matches("7441207b6390054623bc5e659ffe2581356dafc5ec41db44d27de85035000000")).isTrue();
 	}
 	
 	@Test
